fd21409f8636925d8f244f1be24b9db93d4a7ece
modfun <modfun95@gmail.com>
2021-11-13 07:44:40 -0500

    Add playSingle() function to script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function main() {
 
 function computerPlay() {
     //randomlly return either 'rock', 'paper', 'scissors'
-    switch (getRandomInteger(1, 3) ) {
+    switch (getRandomInteger(1, 4) ) {
         case 1: return 'rock'; //return removes the need of break;
         case 2: return 'paper';
         case 3: return 'scissors';
@@ -20,9 +20,39 @@ function computerPlay() {
 function playSingle(playerSelection, computerSelection) {
     //plays a single round of rock, paper, scissors
     //takes two parameters and the function is case-sensitive
+    playerSelection = playerSelection.toLowerCase();
+    let loseMassage = "You Lose! ";
+    let winMassage = "You Win! ";
     //return a string that declares the winner of the round "You Lose! 'infotext'"
+    if (playerSelection === computerSelection ) {
+        console.log("drawing");
+        return "You Draw! " + playerSelection + " draws " + computerSelection;
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return loseMassage + computerSelection + " beats " + playerSelection;
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return winMassage + playerSelection + " beats " + computerSelection;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return winMassage + playerSelection + " beats " + computerSelection;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return loseMassage + computerSelection + " beats " + playerSelection;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return loseMassage + computerSelection + " beats " + playerSelection;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return winMassage + playerSelection + " beats " + computerSelection;
+    }
+    else {
+        console.log("problem at playSingle");
+    }
 }
 
 function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max -min) ) + min;
+    let x = Math.floor(Math.random() * (max -min) ) + min;
+    // console.log("random:" + x);
+    return x;
 }
